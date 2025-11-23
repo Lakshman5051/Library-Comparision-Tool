b0f640bddbf90e3d147e1f9ac6d18bc4725f2e75
Lakshman5051 <lakshmanbabu2001@gmail.com>
2025-11-22 21:37:22 -0600

    Configuration files setup

---
+++ src/main/java/com/project/library_comparison_tool/Config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.project.library_comparison_tool.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*")); // Allow all for now, we'll fix this later
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
